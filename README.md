63caed0c0fff23daee0a4337466860ee0e55e27c
Tyler Allen <tyler.allen@hashicorp.com>
2024-07-30 16:29:39 +0200

    Writing a secret

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # vault-load-testing
 Using locust.io to load test Vault
+
+## To Install
+```bash
+brew install locust
+```
+
+## How To Run
+```bash
+export VAULT_TOKEN=[TOKEN]
+locust -f locustfile.py 
+```
+* Then goto [locust](http://127.0.0.1:8089/)
+* Select host - http://127.0.0.1:8200
---
+++ locustfile.py
@@ -0,0 +1,19 @@
+from locust import HttpUser, between, task
+import os
+
+vault_token = os.getenv('VAULT_TOKEN')
+
+class WriteSecret(HttpUser):
+    wait_time = between(1, 5)
+    
+    def on_start(self):
+        self.client.post(url="/v1/sys/mounts/locust-load-test", 
+                         headers={"X-Vault-Token": vault_token},
+                         json={"type": "kv", "options": {"version": "2"}})
+    
+        
+    @task
+    def about(self):
+        self.client.post(url="/v1/locust-load-test/data/some-key", 
+                         headers={"X-Vault-Token": vault_token},
+                         json={"data": {"hello": "world"}})
